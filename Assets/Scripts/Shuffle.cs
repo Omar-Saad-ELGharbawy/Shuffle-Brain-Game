da92ef9c7493d3c3589f7ab73e58e61e15ab7411
sherif <sherifahmedelsayed2@gmail.com>
2023-12-06 15:17:12 +0200

    Fix shuffling
    - Fix shuffling repitition
    - Remove unused code
    - Add Shuffle tag

--- Assets/Scripts/Shuffle.cs
+++ Assets/Scripts/Shuffle.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
+using static Unity.Burst.Intrinsics.X86.Avx;
 
 public class Shuffle : MonoBehaviour
 {
     List<int> transformList = new List<int>();
-    int[] shuffledList = new int[3];
-    public static int score = 0;
+    List<int> shuffledList = new List<int>();
+
+    public static int levelNum = 0;
     public static int shuffleCount = 5;
     public static float suffleDuration = 1.0f;
 
@@ -17,10 +19,12 @@ public class Shuffle : MonoBehaviour
     public GameObject ball;
     [SerializeField] private List<GameObject> shuffleObjects = new List<GameObject>();
 
+    private void Awake()
+    {
+        GetShufflingObjects();
+    }
     void Start()
     {
-        InitializeList();
-        
     }
 
     void Update()
@@ -30,24 +34,27 @@ public class Shuffle : MonoBehaviour
             StartCoroutine(ShuffleList());
         }
     }
+
+    void GetShufflingObjects()
+    {
+        // Assuming your cups have a specific tag, adjust this based on your setup
+        GameObject[] shuffleObjects = GameObject.FindGameObjectsWithTag("Shuffle");
+
+        foreach (GameObject shuffle in shuffleObjects)
+        {
+            this.shuffleObjects.Add(shuffle);
+            transformList.Add((int)shuffle.transform.position.x);
+        }
+    }
+
     // <summary>
-    // / Shuffle the elememnts in a given list
+    // Shuffle the elememnts in a given list
     // </summary>
     IEnumerator ShuffleList()
     {
-        // List<int> newShuffle = transformList.OrderBy(item => Random.value).ToList();
-        // newShuffle.CopyTo(shuffledList);
-        string stringList2 = string.Join(", ", transformList);
-        string stringList = string.Join(", ", shuffledList);
-        print("Original");
-        print(stringList2);
-        print("Shuffled");
-        print(stringList);
-
-        for (int s = 0; s < shuffleCount; s++)
+        for (int shufflingNumber = 0; shufflingNumber < shuffleCount; shufflingNumber++)
         {
-            List<int> newShuffle = transformList.OrderBy(item => Random.value).ToList();
-            newShuffle.CopyTo(shuffledList);
+            shuffledList = transformList.OrderBy(item => Random.value).ToList();
 
             float elapsedTime = 0f;
             while (elapsedTime < suffleDuration)
@@ -57,23 +64,13 @@ public class Shuffle : MonoBehaviour
                     Vector3 currentPosition = new Vector3(transformList[i], 0f, 0f);
                     Vector3 targetPosition = new Vector3(shuffledList[i], 0f, 0f);
                     shuffleObjects[i].transform.position = Vector3.Lerp(currentPosition, targetPosition, elapsedTime / suffleDuration);
-
                 }
                 elapsedTime += Time.deltaTime;
                 yield return null;
             }
+            transformList = shuffledList;
         }
     }
 
-    void InitializeList()
-    {
-        // Assuming your cups have a specific tag, adjust this based on your setup
-        GameObject[] cupArray = GameObject.FindGameObjectsWithTag("Cup");
 
-        foreach (GameObject cup in cupArray)
-        {
-            shuffleObjects.Add(cup);
-            transformList.Add((int)cup.transform.position.x);
-        }
-    }
 }
